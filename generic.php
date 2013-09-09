5787dda6775c3a588deabf929618981a0028f7a4
henrique <henriquebs@vallery.com.br>
2013-09-09 10:22:23 -0300

    Segundo commit

---
+++ generic.php
@@ -0,0 +1,36 @@
+<?php
+	class genericArray {
+		
+		public function __construct(){
+			return Request::current()->post();
+		}
+		
+		/* Para substituir o foreach */
+		public function foreachArray($post){
+			if(!isset($post)){
+				$post = self::__construct();
+			}
+			
+			/* Serialize */
+			$array['key'] 		= array_keys($post);
+			$array['value'] 	= array_values($post);
+			
+			return $array;
+		}
+		
+		/* Procura um valor de array e retorna sua Key ou Value */
+		public function searchArray($search, $array){
+			return array_search($search, $array, true);
+		}
+		
+		/* Para vizualizar arrays */
+		public function dumpArray($post)
+		{
+			if(!isset($post)){
+				$post = self::__construct();
+			}
+			$post['count'] = count($post);
+			return var_dump($post);
+		}
+	}
+?>
